f7319dad729218c5c9d950ed5b2522b903c75ec6
Don Harper <crabmeat99@gmail.com>
2019-10-08 10:30:35 -0400

    Delete lab7

--- Lab7/Library/Button.h
+++
@@ -1,8 +0,0 @@
-/*
- *  Button.h
- */
-void button_init(void);
-
-bool button_S1_pressed(void);
-
-bool button_S2_pressed(void);
--- Lab7/Library/Encoder.h
+++
@@ -1,4 +0,0 @@
-
-void encoder_init(void);
-int get_left_motor_count();
-int get_right_motor_count();
